2c5772002aac6dfa43fe47b093544594fa455e1d
Michael Wagner <mike.wagner02@gmail.com>
2026-03-06 13:52:02 -0500

    Comments

--- HalloweenBoo.ino
+++ HalloweenBoo.ino
@@ -4,17 +4,22 @@ const int DATA_PIN = 2;
 const CRGB ORANGE = CRGB(237,109,0);
 const CRGB OFF = CRGB(0,0,0);
 
+// Basic function to iterate over every LED in the wire and turn it off
 void empty(CRGB* wire, int count) {
   for (int i = 0; i < count; i++) {
     wire[i] = OFF;
   }
 }
+// Basic function to iterate over every LED in the wire and turn it on
 void fill(CRGB* wire, int count)  {
   for (int i = 0; i < count; i++) {
     wire[i] = ORANGE;
   }
 }
 
+// Whereas the previous simple function contain a loop, the following three
+// functions are designed to be called inside of a loop.
+// The `step` parameter is the loop iteration count
 void fillClockwise(CRGB* wire, int step, int size) {
     if (step > size) return;
     wire[step] = ORANGE;
@@ -34,9 +39,13 @@ typedef void(*fillerFunc)(CRGB*, int, int);
 fillerFunc fillers[] = {&fillFromTop, &fillClockwise, &snakeClockwise};
 const int numFillers = 3;
 
+// There are 240 total LEDs in the string.
 CRGB ALL_LEDS[240];
+// The B is first.  The address of the first LED in B is the address of the first LED in the wire
 CRGB *B = ALL_LEDS;
+// The B has 120 LEDs. The address of the first O is 120 past the start of the wire
 CRGB *FIRST_O = ALL_LEDS + 120;
+// Each O has 60 LEDS.
 CRGB *SECOND_O = ALL_LEDS + 180;
 
 void setup() {
@@ -44,8 +53,13 @@ void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
 }
 void loop() {
+  // Empty the wire
   empty(ALL_LEDS,240);
+  // Select a random filler function
   fillerFunc filler = fillers[random(numFillers)];
+
+  // Call the filler function on each letter.
+  // The B is twice as big as the O's, so we call the filler twice per iteration
   for (int step = 0; step < 60; step++) {
     filler(B,        step*2,   120);
     filler(B,        step*2+1, 120);
